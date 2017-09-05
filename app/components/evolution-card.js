765d3658d1911963da688404bab6f5b91b19d677
Max Antonucci <maxx1128@gmail.com>
2017-09-05 18:03:14 -0400

    Finished basics of evolution card info

--- app/components/evolution-card.js
+++ app/components/evolution-card.js
@@ -8,47 +8,55 @@ export default Ember.Component.extend({
     data.forEach(function(dataItem){
       let evo_result = '';
 
-      if ( dataItem.trigger.name == 'level-up' && !dataItem.min_level ) { evo_result += 'level up '; }
+      if ( dataItem.trigger.name == 'level-up' && !dataItem.min_level ) { evo_result += 'Level up '; }
 
-      if ( dataItem.min_level ) { evo_result += 'level ' + dataItem.min_level + ' '; }
+      if ( dataItem.min_level ) { evo_result += `Level ${dataItem.min_level} `; }
 
-      if ( dataItem.trigger.name == 'trade' ) { evo_result += 'trade '; }
+      if ( dataItem.trigger.name == 'trade' ) { evo_result += 'Trade '; }
 
-      if ( dataItem.trade_species ) { evo_result += 'with ' + dataItem.trade_species.name + ' in party '; }
+      if ( dataItem.item ) { evo_result += `Use ${capitalize(dataItem.item.name)} `; }
+
+      if ( dataItem.trade_species ) { evo_result += `with ${dataItem.trade_species.name} `; }
 
       if ( dataItem.min_happiness ) { evo_result += 'with max happiness '; }
 
       if ( dataItem.min_beauty ) { evo_result += 'with max beauty '; }
 
-      if ( dataItem.item ) { evo_result += 'use ' + dataItem.item.name + ' '; }
+      if ( dataItem.held_item ) { evo_result += `while holding ${capitalize(dataItem.held_item.name)} `; }
 
-      if ( dataItem.held_item ) { evo_result += 'while holding ' + dataItem.held_item.name + ' '; }
+      if ( dataItem.time_of_day ) { evo_result += `during the ${dataItem.time_of_day}time `; }
 
-      if ( dataItem.time_of_day ) { evo_result += 'during the ' + dataItem.time_of_day + 'time '; }
+      if ( dataItem.location ) { evo_result += `at ${capitalize(dataItem.location.name)} `; }
 
-      if ( dataItem.location ) { evo_result += 'at ' + dataItem.location.name + ' '; }
+      if ( dataItem.gender ) { evo_result += `if ${dataItem.gender == 1 ? 'female' : 'male'} `; }
 
-      if ( dataItem.gender ) {
-        let gender_name = (dataItem.gender == 1 ? 'female' : 'male');
+      if ( dataItem.known_move ) { evo_result += `if knows ${capitalize(dataItem.known_move.name)} `; }
 
-        evo_result += 'if ' + gender_name + ' ';
-      }
+      if ( dataItem.known_move_type ) { evo_result += `if knows a ${capitalize(dataItem.known_move_type.name)} move `; }
 
-      if ( dataItem.known_move ) { evo_result += 'if knows ' + dataItem.known_move.name + ' '; }
+      if ( dataItem.party_species ) { evo_result += `with ${capitalize(dataItem.party_species.name)} in party `; }
 
-      if ( dataItem.known_move_type ) { evo_result += 'if knows a ' + dataItem.known_move_type.name + '-type move '; }
-
-      if ( dataItem.party_species ) { evo_result += 'with ' + dataItem.party_species.name + ' in party '; }
-
-      if ( dataItem.min_affection ) { evo_result += 'with ' + dataItem.min_affection + ' affection hearts '; }
+      if ( dataItem.min_affection ) { evo_result += `with ${dataItem.min_affection} affection hearts `; }
 
       if ( dataItem.turn_upside_down ) { evo_result += 'with device upside-down '; }
 
       if ( dataItem.needs_overworld_rain ) { evo_result += 'in a rainy area '; }
 
-      Ember.set(dataItem, 'evo_result', evo_result);
+      if ( dataItem.relative_physical_stats ) {
+        if (dataItem.relative_physical_stats == 1) {
+          evo_result += 'with higher Attack than Defense ';
+        } else if (dataItem.relative_physical_stats == -1) {
+          evo_result += 'with higher Defense than Attack ';
+        } else if (dataItem.relative_physical_stats === 0) {
+          evo_result += 'with equal Attack and Defense ';
+        }
+      }
+
+      Ember.set(dataItem, 'evo_result', evo_result.replace('-', ' '));
     });
 
     return data;
   })
 });
+
+const capitalize = string => string.toString().replace(/\b\w/g, l => l.toUpperCase());
