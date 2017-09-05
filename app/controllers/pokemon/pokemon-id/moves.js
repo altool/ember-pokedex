a75aac3c07707337561717a62581abd8c9c8e11a
Max Antonucci <maxx1128@gmail.com>
2017-09-05 18:45:46 -0400

    Organized data for level-up moves

--- app/controllers/pokemon/pokemon-id/moves.js
+++ app/controllers/pokemon/pokemon-id/moves.js
@@ -5,5 +5,32 @@ export default Ember.Controller.extend({
   lvlMoves: Ember.computed.filter('model.base.moves', function(move) {
 
     return move.version_group_details[0].move_learn_method.name == 'level-up';
-  })
+  }),
+
+  recentMoves: Ember.computed('lvlMoves', function() {
+    let moves = this.get('lvlMoves'),
+        move_name = '',
+        move_level = '',
+        moves_result = [];
+
+    moves.forEach(function(move) {
+      move_name = move.move.name;
+
+      move.version_group_details.forEach(function(version){
+
+        if ( version.version_group.name == 'x-y' ) { move_level = version.level_learned_at; }
+      });
+
+      moves_result.push({
+        'name': move_name.replace('-', ' '),
+        'level': move_level
+      });
+    });
+
+    return moves_result;
+  }),
+
+  sortProperties: ["level:asc"],
+
+  sortedMoves: Ember.computed.sort("recentMoves", "sortProperties")
 });
